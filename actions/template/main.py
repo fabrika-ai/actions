d26461e045aab68bd6050f97f024b75b67bd3693
Aleksey Korshuk <ale-kor02@mail.ru>
2023-11-11 15:45:43 -0800

    setup

--- actions/template/main.py
+++ actions/template/main.py
@@ -5,56 +5,10 @@ from helpers.utils import as_cloud_function
 
 app = FastAPI()
 
-import asyncio
-
-import nest_asyncio
-
 
 @app.get("/hello_world")
 async def get_user_name():
     return {"Hello": "World"}
-
-
-def as_cloud_function(fastapi_app, cloud_function_request):
-    body = cloud_function_request.get_data(parse_form_data=False)
-    scope = {
-        "type": "http",
-        "method": cloud_function_request.method,
-        "headers": [(k.lower().encode(), v.encode()) for k, v in cloud_function_request.headers.items()],
-        "path": cloud_function_request.path,
-        "raw_path": cloud_function_request.path.encode(),
-        "query_string": cloud_function_request.query_string,
-        "scheme": "http",
-        "server": None,
-        "client": None,
-        "root_path": "",
-        "app": fastapi_app,
-    }
-
-    async def receive():
-        return {
-            "type": "http.request",
-            "body": body,
-            "more_body": False
-        }
-
-    response_body = []
-    response_status = 200
-    response_headers = []
-
-    async def send(message):
-        nonlocal response_body, response_status, response_headers
-        if message["type"] == "http.response.start":
-            response_status = message["status"]
-            response_headers = message["headers"]
-        elif message["type"] == "http.response.body":
-            response_body.append(message.get("body", b""))
-
-    nest_asyncio.apply()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fastapi_app(scope, receive, send))
-
-    return (b''.join(response_body), response_status, dict(response_headers))
 
 
 @functions_framework.http
