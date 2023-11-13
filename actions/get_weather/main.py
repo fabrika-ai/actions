f0e2bcca0007b1093a563c02f11fd25a55ead131
Artemii Novoselov <crimeacs@yandex.ru>
2023-11-12 21:06:18 -0500

    fixed weather

--- actions/get_weather/main.py
+++ actions/get_weather/main.py
@@ -7,7 +7,7 @@ from helpers.utils import as_cloud_function
 app = FastAPI()
 
 # Define the Dark Sky API URL and headers
-WEATHER_URL = f"http://api.weatherapi.com/v1/current.json?key=f615ff232b2448d1a8312353231311&q={}&aqi=no"
+WEATHER_URL = "http://api.weatherapi.com/v1/current.json?key=f615ff232b2448d1a8312353231311&q={}&aqi=no"
 
 @app.get("/get_weather/{location}")
 async def read_weather(location: str):
