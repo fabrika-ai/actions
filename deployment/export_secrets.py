cb5039e5d74a3406dc2bd631009271ea1658b4f4
Aleksey Korshuk <ale-kor02@mail.ru>
2023-11-15 19:47:40 -0800

    setup

--- deployment/export_secrets.py
+++
@@ -1,8 +0,0 @@
-def generate_env_vars_string():
-    from actions.api_keys import API_KEYS  # Adjust the import path as necessary
-    env_vars = [f"{key}" + "=${{secrets." + f"{key}" + "}}" for key in API_KEYS]
-    return ",".join(env_vars)
-
-
-if __name__ == "__main__":
-    print(generate_env_vars_string())
